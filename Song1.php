65b01c874168433b97a0a9872ef44e2c610d4cfd
OyongAngelMae <angelmaeoyong.29@gmailcom>
2022-05-09 22:43:34 +0800

    song1 added

---
+++ Song1.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lovely Stones </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Lovely Stones</h1>
+                <p class="lead"> Musics are like stones that has firm, solid and sometines have small composure but not as hard as rock.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+
+                        <?php
+                        
+                        echo "class="text-primary">Song Title here</h2>";
+                        echo " <p class="lead">display the Composer here</p>";
+                        echo "<p>     G     </p>";
+                        echo "<p>In the summertime when the weather is hot,</p>";
+                        echo "<p>you can stretch right up and touch the sky,</p>";
+                        echo "<p>       C       </p>";
+                        echo "<p>when the weather's fine,</p>";
+                        echo "<p>                                     G </p>";
+                        echo "<p>you got women, you got women on your mind.</p>";
+                        echo "<p>       D </p>";
+                        echo "<p>Have a drink, have a drive,</p>";
+                        echo "<p>if her daddy's poor, just do as you feel.</p>";
+                        echo "<p>      C                                             G </p>";
+                        echo "<p>Speed along the lane, do a ton or a ton and twenty-five. </p>";
+                        echo "<p>         D </p>";
+                        echo "<p>When the sun goes down,</p>";
+                        echo "<p>        C                             G </p>";
+                        echo "<p>you can make it make it good in a lay-by.</p>";
+                        echo "<p>                  G </p>";
+                        echo "<p> We're no threat, people, we're not dirty, we're not mean,</p>";
+                        echo "<p> we love everybody but we do as we please. </p>";
+                        echo "<p>           C</p>";
+                        echo "<p>When the weather's fine,</p>";
+                        echo "<p>                                    G</p>";
+                        echo "<p>we go fishing or go swimming in the sea.</p>";
+                        echo "<p>           D</p>";
+                        echo "We're always happy,";
+                        echo "<p>        C                                G</p>";
+                        echo "<p>life's for living, yeah, that's our philosophy.</p>";
+                        echo "          G";
+                        echo "<p>Sing along with us, dee dee dee-dee dee</p>";
+                        echo "<p> Dah dah dah-dah dah yeah we're hap-happy</p>";
+                        echo "<p> C                                 G</p>";
+                        echo "<p> dah dah-dah, di di di di di da da da</p>";
+                        echo "<p>         D</p>";
+                        echo "<p>Da da da da ,</p>";
+                        echo "<p>da   da   da   da   da   da   da   da   da   da   da   da </p>";  
+                        echo "<p>          G</p>";
+                        echo "<p>When the winter's here, yeah, it's party-time,</p>";
+                        echo "<p>bring a bottle, wear your bright clothes, it'll soon be summertime.</p>"
+                        echo "<p>          D</p>";
+                        echo "<p>If she's rich if she's nice,</p>";
+                        echo "<p>            C</p>";
+                        echo "<p>bring your friend and we will all go into Gtown.</p>";
+                        echo "<p>Repeat Verse 1, 2, 3 and then the diddles until fade out.</p>";
+
+
+
+
+
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Angel Mae B. Oyong</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
