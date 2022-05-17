3156bbbbf1b607ff97843cad47bef7a7bc982a30
OyongAngelMae <angelmaeoyong.29@gmailcom>
2022-05-18 07:37:24 +0800

    song5 addded

---
+++ song5.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lovely Stones </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Lovely Stones</h1>
+                <p class="lead"> Musics are like stones that has firm, solid and sometines have small composure but not as hard as rock.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+                        echo "<h2 class="text-primary">Ang Huling El Bimbo</h2>";
+                        echo "<h2 class="lead">Eraserheads</h2>";
+                        echo "<p>Verse:</p>";
+                        echo "<p>G-A7-C-G</p>";
+                        echo "<p>Kmukha mo si Paraluman</p>";
+                        echo "<p>No'ng tayo ay bata pa</p>"
+                        echo "<p>At ang galing-galing mong sumayaw</p>"
+                        echo "<p>  Mapa-Boogie man o Chacha</p>";
+                        echo "<p>Ngunit ang paborito</p>";
+                        echo "<p>Ay pagsayaw mo ng El Bimbo</p>";
+                        echo "<p>Nakakaindak, nakakaaliw</p>";
+                        echo "<p>Nakakatindig balahibo</p>";
+                        echo "<p>Refrain1:</p>";
+                        echo "<p>Em-G-C-D</p>";
+                        echo "<p>Pagkagaling sa 'skwela ay didiretso na sa inyo</p>";
+                        echo "<p>At buong maghapon ay tinuturuan mo ako</p>";
+                        echo "<p>CHORUS:katulad sa intro pero kayo na ang bahala sa strum!!!</p>";
+                        echo "<p>Magkahawak ang ating kamay at walang kamalay-malay</p>";
+                        echo "<p>Na tinuruan mo ang puso ko na umibig na tunay</p>";
+                        echo "<p>Verse:</p>";
+                        echo "<p>G-A7-C-G</p>";
+                        echo "<p>Naninigas aking katawan</p>";
+                        echo "<p>'Pag umikot na ang plaka</p>";
+                        echo "<p>Patay sa kembot ng bewang mo</p>";
+                        echo "<p>At pungay ng 'yong mga mata</p>";
+                        echo "<p>Lumiliwanag ang buhay</p>";
+                        echo "<p>Habang tayo'y magkaakbay</p>";
+                        echo "<p>At dahan-dahang dumudulas</p>";
+                        echo "<p>Ang kamay ko sa makinis mong braso, hooh</p>";
+                        echo "<p>Refrain2:</p>";
+                        echo "<p>Em-G-C-D</p>";
+                        echo "<p>Sana noon pa man ay sinabi na sa iyo, hoh</p>";
+                        echo "<p>Kahit hindi na uso ay ito lang ang alam ko</p>";
+                        echo "<p>(CHORUS)</p>";
+                        echo "<p>katulad sa verse!!!</p>";
+                        echo "<p>La la la la, la la, la la, la la la</p>";
+                        echo "<p>Verse:  </p>";
+                        echo "<p>G-A7-C-G</p>";
+                        echo "<p>Lumipas ang maraming taon</p>";
+                        echo "<p>'Di na tayo nagkita</p>";
+                        echo "<p>Balita ko'y may anak ka na</p>";
+                        echo "<p>Ngunit walang asawa</p>";
+                        echo "<p>Tagahugas ka raw</p>";
+                        echo "<p>At isang gabi'y nasagasaan</p>";
+                        echo "<p>Sa isang madilim na eskinita, hah</p>";
+                        echo "<p>Refrain3: </p>";
+                        echo "<p>Em-G-C-D</p>";
+                        echo "<p>Lahat ng pangarap ko'y biglang natunaw</p>";
+                        echo "<p>Sa panaginip na lang pala kita maisasayaw</p>";
+                        echo "<p>(CHORUS 2x)</p>";
+                        echo "<p>CODA 1:katulad pa rin sa verse!!!!</p>";
+                        echo "<p>La la la la, la la, la la, la la la (2x)</p>";
+
+  ?>
+                      
+                       
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Angel Mae B. Oyong</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
